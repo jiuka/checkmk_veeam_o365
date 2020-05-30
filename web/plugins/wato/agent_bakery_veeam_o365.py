6270a49116c598035fc2c27783dcabf0cc7e892d
Marius Rieder <marius.rieder@durchmesser.ch>
2020-05-30 12:14:45 +0200

    Add bakery integration.

---
+++ web/plugins/wato/agent_bakery_veeam_o365.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+#
+# Copyright (C) 2020  Marius Rieder <marius.rieder@durchmesser.ch>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    DropdownChoice,
+)
+from cmk.gui.plugins.wato import (
+    HostRulespec,
+    rulespec_registry,
+)
+
+try:
+    from cmk.gui.cee.plugins.wato.agent_bakery import (
+        RulespecGroupMonitoringAgentsWindowsAgent
+    )
+except Exception:
+    RulespecGroupMonitoringAgentsWindowsAgent = None
+
+
+def _valuespec_agent_config_veeam_o365():
+    return DropdownChoice(
+        title=_('Veeam for Office 365'),
+        help=_('This will deploy the agent plugin <tt>veeam_o365_status</tt> '
+               'for checking Veeam for Office 365 status.'),
+        choices=[
+            (True, _('Deploy Veeam for Office 365 plugin')),
+            (None, _('Do not deploy Veeam for Office 365 plugin')),
+        ],
+    )
+
+
+if RulespecGroupMonitoringAgentsWindowsAgent is not None:
+    rulespec_registry.register(
+        HostRulespec(
+            group=RulespecGroupMonitoringAgentsWindowsAgent,
+            name='agent_config:veeam_o365',
+            valuespec=_valuespec_agent_config_veeam_o365,
+        ))
