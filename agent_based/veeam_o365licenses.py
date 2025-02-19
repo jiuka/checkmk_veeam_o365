78f57a26a91d53f6360d1468d63fd2fab16aa367
Manuel <manuel.michalski@me.com>
2025-02-19 13:58:21 +0100

    Update veeam_o365licenses.py

--- agent_based/veeam_o365licenses.py
+++ agent_based/veeam_o365licenses.py
@@ -4,9 +4,8 @@
 # Copyright (C) 2020  Marius Rieder <marius.rieder@durchmesser.ch>
 #
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# modify it under the terms of the GNU General Public License;
+# either version 2 of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,9 +16,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-# <<<veeam_o365licenses:sep(9) >>>
-# Valid	28.06.2020 02:00:00	2592000	42	50000
-
 from cmk.agent_based.v2 import (
     Service,
     Result,
@@ -29,31 +25,45 @@ from cmk.agent_based.v2 import (
     CheckPlugin,
 )
 
-
 def discovery_veeam_o365licenses(section):
     for index, line in enumerate(section):
         yield Service(item=str(index))
-
 
 def check_veeam_o365licenses(item, params, section):
     if int(item) >= len(section):
         return
 
-    license_state, license_date, license_validity, \
-        license_used, license_total = section[int(item)]
+    license_state, license_date, license_validity, license_used, license_total = section[int(item)]
 
     if license_state == 'Valid':
-        yield Result(state=State.OK, summary='License is vaild till %s' % license_date)
+        summary_message = f'License is valid till {license_date}' if license_date else 'License has no expiration date'
+        yield Result(state=State.OK, summary=summary_message)
     else:
-        yield Result(state=State.CRIT, summary='License is %s' % license_state)
+        yield Result(state=State.CRIT, summary=f'License is {license_state}')
 
-    yield from check_levels(
-        float(license_validity),
-        levels_lower=params.get('validity', ('fixed', (0, 0))),
-        label='Period of validity' if float(license_validity) > 0 else 'Expired since',
-        render_func=lambda f: render.timespan(f if f > 0 else -f),
-    )
+    # Sicherstellen, dass license_validity nicht leer ist
+    license_validity = section[int(item)][2].strip()  # Entferne Leerzeichen
 
+    if not license_validity or license_validity in ['-1', '']:  # Falls leerer String oder '-1'
+        license_validity = None
+    else:
+        try:
+            license_validity = float(license_validity)
+            if license_validity < 0:  # Falls der Wert ungültig ist
+                license_validity = None  # Markieren als nicht vorhanden
+        except ValueError:
+            license_validity = None  # Falls die Umwandlung fehlschlägt
+
+    if license_validity is not None and license_validity > 0:
+        label = 'Period of validity'
+        validity_message = render.timespan(license_validity)
+    else:
+        label = 'License does not expire'
+        validity_message = label
+
+    yield Result(state=State.OK, summary=validity_message)
+
+    # Handling license usage
     license_used = int(license_used)
     license_total = int(license_total)
 
@@ -67,17 +77,18 @@ def check_veeam_o365licenses(item, params, section):
         case _:
             levels = ('fixed', (int(license_total), int(license_total)))
 
-    yield from check_levels(license_used,
-                            label="used",
-                            render_func=lambda v: f"{v:d}",
-                            levels_upper=levels,
-                            metric_name='licenses',
-                            boundaries=(0, int(license_total)))
-
+    yield from check_levels(
+        license_used,
+        label="used",
+        render_func=lambda v: f"{v:d}",
+        levels_upper=levels,
+        metric_name='licenses',
+        boundaries=(0, int(license_total))
+    )
 
 check_plugin_veeam_o365jobs = CheckPlugin(
     name='veeam_o365licenses',
-    service_name = 'VEEAM O365 License %s',
+    service_name='VEEAM O365 License %s',
     discovery_function=discovery_veeam_o365licenses,
     check_function=check_veeam_o365licenses,
     check_ruleset_name='veeam_o365licenses',
